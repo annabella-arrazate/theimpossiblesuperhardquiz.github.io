b378feef176bbadec326e9867a481fd4aabe90c8
annabella-arrazate <annabellaarrazate01@gmail.com>
2025-05-29 20:18:50 +0000

    basics

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+ function showAlert() {
+    prompt("Answer?")
+  } 
+  
+  let userInput = prompt 
+  if (userInput === "09/22/2010") {
+    alert("Correct.")
+  } if (userInput !=== "09/22/2010") {
+    alert("Wrong!")
+  }
